bba72241cb4b985c60431ffe49a6366add5fe253
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-09-27 07:26:35 +0100

    consertando botao about me

--- src/Components/ButtonAboutMe/ButtonAboutMe.js
+++ src/Components/ButtonAboutMe/ButtonAboutMe.js
@@ -1,34 +1,43 @@
+import React, {useState} from 'react';
 import AboutMeCss from '../AboutMe/AboutMe.module.css'
 import ButtonMe from '../ButtonAboutMe/ButtonAboutMe.module.css'
 
 
-
-function OpenText(){
-   let text = document.getElementById("text");
-   text.innerHTML = `
-   <div className={Text.botao__quem_sou}>
-      <p className={TextButton.texto__quem_sou}>   Olá, meu nome é {name}, sou natural de Goiânia - Goiás e formado em Gestão de Pessoas. 
-      Tenho {age} anos e estou migrando para a área de tecnologia.
-      Venho preparando-me para ser um Desenvolvedor Front-End, aprendendo diariamente com a conclusão de cursos e exercícios práticos.
-         Prática essa que me capacitou a desenvolver projetos como esse, utilizando {tecnologias.markup}, {tecnologias.style} com o framework {tecnologias.frameworkCss} e a linguagem de programação {tecnologias.language}.
-         Procurarei aprender ainda mais para incluir novas funcionalidades ao projeto.</p>
-         <button type="button" onclick="fechar()" className="btn btn-warning">Fechar</button>
-   </div>
-   `
-
-}
-
-
-  
-
 function ButtonAboutMe(){
+   const [mostrarNovaDiv, setMostrarNovaDiv] = useState(false);
 
-   return(
-      <div id="text" className={AboutMeCss.apresentacao}>
-         <button type="button" onClick={OpenText} className={`btn btn-warning ${ButtonMe.botao__quem_sou} `}>About Me</button>
-      </div>
-   )
-    
+   function AbrirNovaDiv(){
+      setMostrarNovaDiv(true);
+   }
+
+   function Fechar(){
+      setMostrarNovaDiv(false);
+   }
+
+   return (
+   <div>
+      <button type="button" onClick={AbrirNovaDiv} className={`btn btn-danger ${AboutMeCss.apresentacao} ${ButtonMe.botao__quem_sou}`}>
+         About Me
+      </button>
+
+      {mostrarNovaDiv && (
+      <div>
+          <div id='text'>
+        <p>   Olá, meu nome é Wellington, sou natural de Goiânia - Goiás e formado em Gestão de Pessoas. 
+        Tenho 35 anos e estou migrando para a área de tecnologia.
+        Venho preparando-me para ser um Desenvolvedor Front-End, aprendendo diariamente com a conclusão de cursos e exercícios práticos.
+            Prática essa que me capacitou a desenvolver projetos como esse, utilizando html, Css, Js e React .
+            Procurarei aprender ainda mais para incluir novas funcionalidades ao projeto.</p>
+
+    </div>
+          <button type="button" onClick={Fechar} className={`btn btn-danger ${ButtonMe.botao__quem_sou}`}>
+            Close About Me
+          </button>
+        </div>
+      )}
+   </div>
+  );
 }
+
 
 export default ButtonAboutMe
