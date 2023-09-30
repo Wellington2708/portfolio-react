b043b0ddb4ab6905389e25b9256c8c908b22b241
Wellington2708 <wellingtondevelopercontact@gmail.com>
2023-09-30 22:03:32 +0100

    refaturando contacts section

--- src/Components/SectionContacts/Contacts.js
+++ src/Components/SectionContacts/Contacts.js
@@ -1,26 +1,28 @@
 import ContactMe from './Contacts.module.css'
+import {BsFacebook} from 'react-icons/bs'
+import {SiGithub} from 'react-icons/si'
+import {SiGmail} from 'react-icons/si'
+import {FaLinkedin} from 'react-icons/fa'
 
 
 function Contacts(){
     return (
-    <section id="contacts" class={ContactMe.contatos}>
+    <section id="contacts" className={ContactMe.contatos}>
        
-        <h2 class="titulo__contatos">Contatos:</h2>
-        <div class="contatos">
+        <h2 className={ContactMe.titulo__contatos}>Contatos:</h2>
+        <div className={ContactMe.lista__contatos}>
+                <ul className={ContactMe.list}>
+                    <a className={`${ContactMe.item__contatos} ${ContactMe.link__email}`} href="mailto:wellingtondevelopercontact@gmail.com">
+                    <SiGmail className={ContactMe.fa_envelope}/> Email</a>
+                    <a className={`${ContactMe.item__contatos} ${ContactMe.link__linkedin}`}
+                        href="https://www.linkedin.com/in/wellington-junior-41513874/">
+                    <FaLinkedin/> Linkedin</a>
+                    <a className={`${ContactMe.item__contatos} ${ContactMe.link__github}`} href="https://github.com/Wellington2708">
+                    <SiGithub className={ContactMe.github}/> Github</a>   
+                    <a className={`${ContactMe.item__contatos} ${ContactMe.link__facebook}`} href="https://www.facebook.com/jhonnyn010">
+                    <BsFacebook/> Facebook</a>
 
-            <div class="lista__contatos">
-                <ul>
-                    <a class="item__contatos link__email" href="mailto:wellingtondevelopercontact@gmail.com"><i
-                            class="fa-solid fa-envelope"></i>Email</a>
-                    <a class="item__contatos link__linkedin"
-                        href="https://www.linkedin.com/in/wellington-junior-41513874/"><i
-                            class="fa-brands fa-linkedin"></i>Linkedin</a>
-                    <a class="item__contatos link__github" href="https://github.com/Wellington2708"><i
-                            class="fa-brands fa-github"></i>Github</a>
-                    <a class="item__contatos link__facebook" href="https://www.facebook.com/jhonnyn010">
-                        <i class="fa-brands fa-facebook"></i>Facebook</a>
                 </ul>
-            </div>
         </div>
      
     </section>
