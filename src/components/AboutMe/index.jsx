b22e4ac5936ad082763411b7b48661b433afc49c
Wellington2708 <wellingtondevelopercontact@gmail.com>
2024-09-15 21:02:16 +0100

    css adjustments and changing text about me

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -7,7 +7,7 @@ import Top from  '../Top';
 
 function AboutMe() {
   const [showAboutMe, setShowAboutMe] = useState(false);
-  const age = 35;
+  const age = 36;
 
   const toggleAboutMe = () => {
     setShowAboutMe(!showAboutMe);
@@ -25,11 +25,11 @@ function AboutMe() {
       {showAboutMe && (
         <div className={styles.close__button}>
           <p className={styles.text__whoIam}>
-            Hello, my name is Wellington Luis, I am Brazilian and I'm {age} years old. I have a degree in People Management and today I am in a career transition to the technology area.
-             I am a Frontend Developer, and have knowledge in <span style={{ fontSize: '20px', color: '#4d54e7' }}>HTML, CSS, Javascript, Bootstrap,
-              Git, Github, Node.js, React JS.</span> I have experience with document control, typing, production, teamwork, attention to deadlines for 
-              delivery of products and communication. I leave my <a href="https://www.github.com/Wellington2708" target="_blank" rel="noopener noreferrer">
-              Github Here</a> to better demonstrate my work. Welcome to my portfolio.
+              Hello, my name is Wellington Luis, I am Brazilian and I'm {age} years old.
+              I'm Freelance Frontend Developer with 2 years of experience, and have knowledge in <em style={{ fontSize: '16px', color: '#4d54e7' }}>HTML, CSS, Javascript, Bootstrap,
+              Git, Github, Node.js, React JS.</em><br></br> I have experience with document control, typing, production, teamwork, attention to deadlines for 
+              delivery of products and communication.<br></br> Some of the developed projects can be seen on my <a href="https://www.github.com/Wellington2708" target="_blank" rel="noopener noreferrer">
+              Github </a> to better demonstrate my work. 
           </p>
         </div>
       )}
